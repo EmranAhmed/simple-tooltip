2c506e7e8b25fe4cd6a201b52cf4b9f5bbb1f734
Emran Ahmed <emran.bd.08@gmail.com>
2023-05-24 04:26:41 +0600

    Init :tada:

---
+++ scripts.js
@@ -0,0 +1,54 @@
+;(function (window) {
+  const items = document.querySelectorAll('.item')
+
+  items.forEach((item) => {
+
+    item.addEventListener('mouseenter', (event) => {
+      const rect = item.getBoundingClientRect()
+      const afterElement = window.getComputedStyle(item, ':after')
+      const beforeElement = window.getComputedStyle(item, ':before')
+      const offset = 2
+
+      const afterTopWidth = parseInt(
+        afterElement.getPropertyValue('border-top-width'), 10)
+
+      const beforeHeight = parseInt(beforeElement.getPropertyValue('height'),
+        10)
+
+      const beforeWidth = parseInt(beforeElement.getPropertyValue('width'),
+        10)
+
+      const topPosition = beforeHeight + afterTopWidth + offset
+
+      item.classList.toggle('tooltip-move-to-bottom',
+        rect.y < topPosition)
+
+      item.classList.toggle('tooltip-move-to-top',
+        rect.y > topPosition)
+
+      const width = (beforeWidth / 2)
+      const position = rect.left + (rect.width / 2)
+
+      // left
+      const left = width - position
+      const isLeft = width > position
+
+      // right
+      const computedRight = width + position
+      const isRight = document.body.clientWidth < computedRight
+      const right = document.body.clientWidth - computedRight
+
+      item.style.setProperty('--dynamic-position', `0px`)
+
+      if (isLeft) {
+        item.style.setProperty('--dynamic-position', `${left + offset}px`)
+      }
+
+      if (isRight) {
+        item.style.setProperty('--dynamic-position', `${right - offset}px`)
+      }
+    })
+  })
+
+}(window))
+
