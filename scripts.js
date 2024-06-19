4ec6e44f9b779dc939583e3d63d30ca690eb346b
Emran Ahmed <emran.bd.08@gmail.com>
2024-06-20 01:15:24 +0600

    update markup and add transition

--- scripts.js
+++ scripts.js
@@ -1,55 +1,65 @@
 ;(function (window) {
-  const items = document.querySelectorAll('.item')
+  const items = document.querySelectorAll('[data-storepress-tooltip]')
+
+  function setPosition (event) {
+
+    const element = event.target
+
+    element.style.removeProperty('--_tooltip-position')
+
+    const rect = element.getBoundingClientRect()
+    const style = window.getComputedStyle(element)
+
+    const tooltipHeight = parseInt(style.getPropertyValue('--tooltip-height'), 10)
+    const tooltipWeight = parseInt(style.getPropertyValue('--tooltip-width'), 10)
+    const tooltipAngle = parseInt(style.getPropertyValue('--tooltip-angle'), 10)
+    const tooltipEdge = parseInt(style.getPropertyValue('--tooltip-edge'), 10)
+    const tooltipOffset = parseInt(style.getPropertyValue('--tooltip-offset'), 10)
+
+    const calculatedPosition = tooltipHeight + tooltipAngle + tooltipEdge + tooltipOffset
+
+    element.classList.toggle('storepress-tooltip-position-bottom', rect.top < calculatedPosition)
+
+    element.classList.toggle('storepress-tooltip-position-top', rect.top > calculatedPosition)
+
+    const width = (tooltipWeight / 2)
+    const position = rect.left + (rect.width / 2)
+
+    // left
+    const left = width - position
+    const isLeft = width > position
+
+    // right
+    const computedRight = width + position
+    const isRight = document.body.clientWidth < computedRight
+    const right = document.body.clientWidth - computedRight
+
+    if (isLeft) {
+      element.style.setProperty('--_tooltip-position', `${left + tooltipEdge}px`)
+    }
+
+    if (isRight) {
+      element.style.setProperty('--_tooltip-position', `${right - tooltipEdge}px`)
+    }
+  }
+
+  function resetPosition (event) {
+    const element = event.target
+
+    element.style.removeProperty('--_tooltip-position')
+    element.classList.remove('storepress-tooltip-visible', 'storepress-tooltip-position-top', 'storepress-tooltip-position-bottom')
+  }
 
   items.forEach((item) => {
 
-    item.addEventListener('mouseenter', (event) => {
+    if (!item.hasAttribute('tabindex')) {
+    //  item.setAttribute('tabindex', 0)
+    }
 
-      const rect = item.getBoundingClientRect()
-
-      const afterElement = window.getComputedStyle(item, ':after')
-      const beforeElement = window.getComputedStyle(item, ':before')
-      const offset = 2
-
-      const afterTopWidth = parseInt(
-        afterElement.getPropertyValue('border-top-width'), 10)
-
-      const beforeHeight = parseInt(beforeElement.getPropertyValue('height'),
-        10)
-
-      const beforeWidth = parseInt(beforeElement.getPropertyValue('width'),
-        10)
-
-      const topPosition = beforeHeight + afterTopWidth + offset
-
-      item.classList.toggle('tooltip-move-to-bottom',
-        rect.top < topPosition)
-
-      item.classList.toggle('tooltip-move-to-top',
-        rect.top > topPosition)
-
-      const width = (beforeWidth / 2)
-      const position = rect.left + (rect.width / 2)
-
-      // left
-      const left = width - position
-      const isLeft = width > position
-
-      // right
-      const computedRight = width + position
-      const isRight = document.body.clientWidth < computedRight
-      const right = document.body.clientWidth - computedRight
-
-      item.style.setProperty('--dynamic-position', `0px`)
-
-      if (isLeft) {
-        item.style.setProperty('--dynamic-position', `${left + offset}px`)
-      }
-
-      if (isRight) {
-        item.style.setProperty('--dynamic-position', `${right - offset}px`)
-      }
-    })
+   // item.addEventListener('focusin', setPosition)
+   // item.addEventListener('focusin', setPosition)
+    // item.addEventListener('pointerout', resetPosition)
+   item.addEventListener('pointerenter', setPosition)
   })
 
 }(window))
